7823bcf2399af8d7663c5a57f40c3dcf883b7d47
Jordy Corrales <corrales.alexanderjc@gmail.com>
2025-04-17 12:04:42 -0500

    ejercicios en clase

--- index.py
+++ index.py
@@ -3,3 +3,19 @@ with open("informacion.txt" ,"r", encoding="utf-8") as file:
 
 for i, linea in enumerate(lineas, start=1):
     print(linea.strip())
+
+print("---------------------")
+lineas = [r.strip() for r in lineas]
+print(lineas)
+print("----------------------")
+
+for l in lineas:
+    print(l)
+
+print("----------------------")
+lineas = lineas[1:]
+for l in lineas:
+    apellido = l[0]
+    if apellido[0] == "B":
+        print(l)
+
